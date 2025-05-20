c05ccb96c4bfb9229dfdc5051b1b4a842d59551a
pmateo0 <pabgas.work@gmail.com>
2025-05-20 16:51:33 +0200

    Testing docker

--- src/main/java/com/pruebaTecnica/pokemon_api/PokemonApiApplication.java
+++ src/main/java/com/pruebaTecnica/pokemon_api/PokemonApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-
+/**/
 @SpringBootApplication(
 		exclude = {
 				DataSourceAutoConfiguration.class,
